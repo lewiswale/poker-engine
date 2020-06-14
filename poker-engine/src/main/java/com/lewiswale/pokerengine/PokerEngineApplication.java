e57d16725c688ee026c1431dfb40edc19f50eedb
Lewis Wale <lew.wale@gmail.com>
2020-06-14 11:16:25 +0100

    made spring app

---
+++ poker-engine/src/main/java/com/lewiswale/pokerengine/PokerEngineApplication.java
@@ -0,0 +1,13 @@
+package com.lewiswale.pokerengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerEngineApplication.class, args);
+	}
+
+}
---
+++ poker-engine/src/test/java/com/lewiswale/pokerengine/PokerEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewiswale.pokerengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
