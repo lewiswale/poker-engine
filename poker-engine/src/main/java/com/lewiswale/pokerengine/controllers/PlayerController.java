fccf377f2a463fe5ad1faf784e7aa336591f8e15
Lewis Wale <lew.wale@gmail.com>
2020-06-14 17:38:42 +0100

    clean up

--- poker-engine/src/main/java/com/lewiswale/pokerengine/controllers/PlayerController.java
+++ poker-engine/src/main/java/com/lewiswale/pokerengine/controllers/PlayerController.java
@@ -3,11 +3,9 @@ package com.lewiswale.pokerengine.controllers;
 import com.lewiswale.pokerengine.model.Card;
 import com.lewiswale.pokerengine.services.GameEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
